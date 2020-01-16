dd3f56d37a493cfc894c2a03e0627a6a601e27aa
Alberto Corona <muqui@hotmail.com>
2020-01-15 18:22:55 -0600

    fix mysql disconnects

--- app/model/db.js
+++ app/model/db.js
@@ -1,9 +1,9 @@
 //'user strict';
-
+//const pool = require('generic-pool');
 var mysql = require('mysql');
 
 
-//Conexion a Mysql
+/*
 
 const connection = mysql.createConnection({
    
@@ -16,7 +16,7 @@ const connection = mysql.createConnection({
    user     : 'root',
    password : 'Fedora12',
    database : 'asistencia'
- */
+ 
   });
 
 
@@ -24,5 +24,39 @@ const connection = mysql.createConnection({
     console.log("error conexion base de datos :  " + err);
     if (err) throw err;
 });
+*/
+
+var db_config = {
+  host     : 'localhost',
+   user     : 'root',
+   password : 'Fedora12',
+   database : 'asistencia'
+};
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+  connection.connect(function(err) {              // The server is either down
+  if(err) {                                     // or restarting (takes a while sometimes).
+          console.log('error when connecting to db:', err);
+          setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+          }                                     // to avoid a hot loop, and to allow our node script to
+   });                                     // process asynchronous requests in the meantime.
+                                           // If you're also serving http, display a 503 error.  
+                                           
+ connection.on('error',function(err){
+   console.log('db error', err);
+   if(err.code === 'PROTOCOL_CONNECTION_LOST'){
+    handleDisconnect();   
+   }
+   else{
+    throw err;     
+   }
+ }
+
+ );                                          
+}
+
+handleDisconnect();
 
 module.exports = connection;
