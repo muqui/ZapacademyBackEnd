03b576ecc10969cbcb31b9200819d35ffc0f6540
Alberto Corona <muqui@hotmail.com>
2020-01-16 18:07:54 -0600

    fix database connect

--- app/model/db.js
+++ app/model/db.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql')
 var pool = mysql.createPool({
     connectionLimit: 10,
-    host     : 'bied0ycmppx0keqba99d-mysql.services.clever-cloud.com',
-    user     : 'ueqm39ayixoqyrc6',
-    password : 'oCmq7FZk6EkuWbOMW3Ik',
-    database : 'bied0ycmppx0keqba99d'
+    host     : 'bu4pcx41i9rxsnguus2p-mysql.services.clever-cloud.com',
+    user     : 'uxyw1knbrsy0eksa',
+    password : 'pt6CIZ7XJbjFpukgfwjK',
+    database : 'bu4pcx41i9rxsnguus2p'
 })
 pool.getConnection((err, connection) => {
     if (err) {
@@ -23,70 +23,3 @@ pool.getConnection((err, connection) => {
 })
 module.exports = pool
 
-
-
-//'user strict';
-//const pool = require('generic-pool');
-
-/*
-var mysql = require('mysql');
-
-
-
-
-const connection = mysql.createConnection({
-   
-  host     : 'bied0ycmppx0keqba99d-mysql.services.clever-cloud.com',
-    user     : 'ueqm39ayixoqyrc6',
-    password : 'oCmq7FZk6EkuWbOMW3Ik',
-    database : 'bied0ycmppx0keqba99d'
-   /*
-   host     : 'localhost',
-   user     : 'root',
-   password : 'Fedora12',
-   database : 'asistencia'
- 
-  });
-
-
-  connection.connect(function(err) {
-    console.log("error conexion base de datos :  " + err);
-    if (err) throw err;
-});
-
-
-var db_config = {
-  host     : 'bied0ycmppx0keqba99d-mysql.services.clever-cloud.com',
-    user     : 'ueqm39ayixoqyrc6',
-    password : 'oCmq7FZk6EkuWbOMW3Ik',
-    database : 'bied0ycmppx0keqba99d'
-};
-
-function handleDisconnect() {
-  connection = mysql.createConnection(db_config); // Recreate the connection, since
-                                                  // the old one cannot be reused.
-  connection.connect(function(err) {              // The server is either down
-  if(err) {                                     // or restarting (takes a while sometimes).
-          console.log('error when connecting to db:', err);
-          setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-          }                                     // to avoid a hot loop, and to allow our node script to
-   });                                     // process asynchronous requests in the meantime.
-                                           // If you're also serving http, display a 503 error.  
-                                           
- connection.on('error',function(err){
-   console.log('db error', err);
-   if(err.code === 'PROTOCOL_CONNECTION_LOST'){
-    handleDisconnect();   
-   }
-   else{
-    throw err;     
-   }
- }
-
- );                                          
-}
-
-handleDisconnect();
-
-module.exports = connection;
-*/
