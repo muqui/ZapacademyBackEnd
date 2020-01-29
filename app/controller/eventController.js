d9dc5eacc7822433d33737853c53f820dc5f9df9
Alberto Corona <muqui@hotmail.com>
2020-01-29 16:56:13 -0600

    add event-beneficiary

--- app/controller/eventController.js
+++ app/controller/eventController.js
@@ -12,12 +12,7 @@ exports.create =   async function (req, res) {
 
     var fecha_inicio = post_data.fecha_inicio;
     var fecha_fin =  post_data.fecha_fin;
-    var nombre = post_data.nombre;
-   
-   
-    
-    
-       
+    var nombre = post_data.nombre; 
         const id = req.session.user_id ;
         console.log('RECUPEAR SESSION ID = ', id);
         mysqlConexion.query('INSERT INTO event(fechaInicio, fechafin, nombre, user_id) VALUES (?,?,?,?)', [fecha_inicio,fecha_fin, nombre, id], function(err,result,fields){
@@ -27,8 +22,21 @@ exports.create =   async function (req, res) {
             });
             res.json('Register success');
         })
-       
-    
+};
 
+exports.beneficiario =   async function (req, res) {
+    var post_data = req.body;
 
+    var beneficiary_id = post_data.beneficiary_id;
+    var event_id =  post_data.event_id;
+    var nombre = post_data.nombre; 
+        const id = req.session.user_id ;
+        console.log('RECUPEAR SESSION ID = ', id);
+        mysqlConexion.query('INSERT INTO beneficiaryEvent(beneficiary_id, event_id) VALUES (?,?)', [beneficiary_id,event_id], function(err,result,fields){
+            mysqlConexion.on('error', function(err){
+                console.log('[MySQL ERROR]', err);
+                res.json('register error: ', err)
+            });
+            res.json('Register success');
+        })
 };
--- app/routes/eventRoute.js
+++ app/routes/eventRoute.js
@@ -10,7 +10,8 @@ const event_controller = require('../controller/eventController');
 // a simple test url to check that all of our files are communicating correctly.
 router.get('/test', event_controller.test);
 // Crea Un  evento
-router.post('/',authorize(), event_controller.create);
+router.post('/',authorize(), event_controller.create);  // Crea un evento
+router.post('/beneficiario',authorize(), event_controller.beneficiario);  //Asigna beneficiarios a un evento  Relacion muchos a muchos
 
 
 module.exports = router;
