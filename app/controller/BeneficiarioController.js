23c5c0c7ae3dc5a0bbd0bcef75f92af6fd9621f4
Alberto Angel Corona Navarro <muqui@localhost.localdomain>
2020-01-31 21:01:48 -0600

    get Beneficiary by ID event and CURP beneficiary

--- app/controller/BeneficiarioController.js
+++ app/controller/BeneficiarioController.js
@@ -108,13 +108,17 @@ exports.lista = function (req, res) {
 };
 
 
-//Regresa beneficiario si esta en la lista del eventa para registrar asistencia
+/*
+Regresa beneficiario si esta en la lista del eventa para registrar asistencia
+Campos recibidos : curp del beneficiario, id del evento
+*/
+
 exports.beneficiarioCurpEvento = function (req, res) {
-    var curp = req.params.curp;  //recibe el id  del Beneficiario
+    var curp = req.params.curp;  //recibe la CURP del Beneficiario
     var evento = req.params.evento;  // recibe el id de evento
     
 
-    mysqlConexion.query('select b.* from beneficiary as b JOIN beneficiaryEvent as be ON b.id = be.beneficiary_id  JOIN event as e ON e.id = be.event_id where e.id = ? and b.id = ?', [evento, curp ], function(err,result,fields){
+    mysqlConexion.query('select b.* from beneficiary as b JOIN beneficiaryEvent as be ON b.id = be.beneficiary_id  JOIN event as e ON e.id = be.event_id where e.id = ? and b.CURP = ?', [evento, curp ], function(err,result,fields){
         mysqlConexion.on('error', function(err){
             console.log('[MySQL ERROR]', err);
             res.json('register error: ', err)
